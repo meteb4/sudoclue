3475eacc55ed19c28f6998cf89c64ffea75a61db
Metehan Kan <mete@MacBook-Air-de-Metehan.local>
2022-10-16 21:50:24 +0200

    Solving algorithm looping

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let M = [
   //
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 0, 0, 0, 0],
   //
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -30,7 +30,7 @@ class Cell {
     tmp.style.boxSizing = "border-box";
     if (this.isAlternated) tmp.style.background = "#ffffff";
     else tmp.style.background = "#dddddd";
-    tmp.style.fontSize = "3rem";
+    tmp.style.fontSize = "1rem";
     tmp.style.textAlign = "center";
     tmp.style.caretColor = "transparent";
     tmp.value = 0;
@@ -38,10 +38,12 @@ class Cell {
     cell.appendChild(tmp);
     // Regulates user input into the cell
     cell.addEventListener("input", (e) => {
-      if (e.data != parseInt(e.data)) tmp.value = "";
-      if (tmp.value.length > 1) {
-        // M[i % 9][Math.floor((i - (i % 9)) / 9)] = parseInt(e.data);
-        tmp.value = e.data;
+      if (e.data != parseInt(e.data)) tmp.value = 0;
+      else {
+        tmp.value = parseInt(e.data);
+        let i = this.index % 9;
+        let j = parseInt((this.index - i) / 9);
+        M[j][i] = parseInt(tmp.value);
       }
     });
     return cell;
@@ -62,18 +64,11 @@ class Row {
 
 // The Sudoku board requires (3x3)*3*3 cells
 
-function isEven(n) {
-  if (n % 2 == 0) {
-    return True;
-  }
-  return False;
-}
-
 class Sudoku {
   render() {
     for (let i = 0; i < 81; i++) {
       document
-        .getElementById("r" + Math.floor(i / 9).toString())
+        .getElementById("r" + parseInt(i / 9).toString())
         .appendChild(new Cell(50, 50, false, i).render());
     }
   }
@@ -83,15 +78,70 @@ for (let i = 0; i < 9; i++) {
   document.getElementById("main").appendChild(new Row(i).render());
 }
 
+let c = 0;
+
 new Sudoku().render();
 
-let BTN = document.getElementById("sub");
-
-BTN.addEventListener("click", (e) => {
-  for (let i = 0; i < 81; i++) {
-    M[i % 9][Math.floor((i - (i % 9)) / 9)] = document.getElementById(
-      "r" + i.toString()
-    ).value;
+class Solver {
+  constructor(board) {
+    this.board = board;
   }
-});
-console.log(M);
+  checkRow(v, i) {
+    for (let j = 0; j < 9; j++) {
+      if (this.board[i][j] == v) return false;
+    }
+    return true;
+  }
+  checkColumn(v, j) {
+    for (let i = 0; i < 9; i++) {
+      if (this.board[i][j] == v) return false;
+    }
+    return true;
+  }
+  checkSquare(v, i, j) {
+    for (let m = parseInt(i / 3) * 3; i < parseInt(i / 3) * 3 + 3; i++) {
+      for (let n = parseInt(j / 3) * 3; i < parseInt(j / 3) * 3 + 3; i++) {
+        if (this.board[m][n] == v) return false;
+      }
+    }
+    return true;
+  }
+  solve(pos) {
+    c++;
+    let i = pos % 9;
+    let j = parseInt((pos - i) / 9);
+    if (c > 100) {
+      console.log(c);
+      return;
+    }
+    if (pos == 81) {
+      console.log(this.board);
+      return true;
+    } else if (this.board[i][j] != 0) {
+      if (this.solve(pos + 1)) return true;
+      else return False;
+    } else if (this.board[i][j] == 0) {
+      for (let k = 1; k < 10; k++) {
+        if (
+          this.checkRow(k, i) &&
+          this.checkColumn(k, j) &&
+          this.checkSquare(k, i, j)
+        ) {
+          this.board[i][j] = k;
+          if (this.solve(pos + 1)) return true;
+          else this.board[i][j] = 0;
+        }
+      }
+    }
+    return false;
+  }
+}
+
+function handleSolver() {
+  let button = document.getElementById("sub");
+  let x = new Solver(M);
+  button.addEventListener("click", (e) => {
+    x.solve(0);
+  });
+}
+// handleSolver();
